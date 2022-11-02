fa94b06e6111e03ee480d1f327844381b8b64e41
Artur Y <artur@artyorsh.me>
2022-11-02 15:18:34 +0100

    sync gh-cli config

--- git/init.sh
+++ git/init.sh
@@ -1,2 +1,3 @@
 ln -sfv $(readlink -f ./git/.gitconfig) "$HOME/.gitconfig"
 ln -sfv $(readlink -f ./git/.gitignore_global) "$HOME/.gitignore_global"
+ln -sfv $(readlink -f ./git/.gh_config.yml) "$HOME/.config/config.yml"
