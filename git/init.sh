2e8b7cfe660c1a4150c0f4b4264acf73fd6b1029
Artur Y <artur@artyorsh.me>
2023-01-19 23:10:02 +0100

    fix gh cli config location

--- git/init.sh
+++ git/init.sh
@@ -1,3 +1,3 @@
 ln -sfv $(readlink -f ./git/.gitconfig) "$HOME/.gitconfig"
 ln -sfv $(readlink -f ./git/.gitignore_global) "$HOME/.gitignore_global"
-ln -sfv $(readlink -f ./git/.gh_config.yml) "$HOME/.config/config.yml"
+ln -sfv $(readlink -f ./git/.gh_config.yml) "$HOME/.config/gh/config.yml"
