82ebee192bf4c9ffd147c9cbadd5f2030612e4c9
Artur Y <artur@artyorsh.me>
2024-11-13 14:42:18 +0100

    restore vscode/init.sh

---
+++ vscode/init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+current_dir=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+VSCODE_PREFS_DIR="$HOME/Library/Application Support/Code/User"
+
+# Default VSCode settings location is not available initially
+mkdir -p $VSCODE_PREFS_DIR
+
+# Wait for directory to become available
+sleep 1
+
+ln -sfv "$current_dir/settings.json" "$VSCODE_PREFS_DIR/settings.json"
+ln -sfv "$current_dir/keybindings.json" "$VSCODE_PREFS_DIR/keybindings.json"
+
+cat "$current_dir/extensions" | xargs -n1 code --install-extension
